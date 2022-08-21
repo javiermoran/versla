d0382d933b17c9910b4da0dae347070189398b33
Porto Hamwich <javier.moran.dev@gmail.com>
2022-08-20 21:22:40 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.scss';
+import React, { ReactElement } from 'react';
+
+function App(): ReactElement {
+  return <div className='App'>Versla app</div>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/index.scss';
+import App from './App';
+
+const rootElement: Element = document.getElementById('root') as Element;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
